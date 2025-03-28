d311e54b9ddc9751a2818242b021a3c4d840acdc
Hamza <hamzaahmad8644@gmail.com>
2025-03-29 03:04:40 +0500

    Make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -66,7 +66,6 @@ document.querySelectorAll(".js-add-to-cart").forEach((button) => {
         matchingItem = item;
       }
     });
-    console.log(matchingItem);
 
     if (matchingItem) {
       matchingItem.quantity += 1;
@@ -77,6 +76,12 @@ document.querySelectorAll(".js-add-to-cart").forEach((button) => {
       });
     }
 
-    console.log(cart);
+    let cartQuantity = 0;
+
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    });
+
+    document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
   });
 });
